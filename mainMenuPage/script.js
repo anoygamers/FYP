7d5707b13fa434e92908ae5ae68f5f509ba3c427
anoygamers <anoygamers@gmail.com>
2024-10-14 12:19:26 +0800

    Baik pulih main menu dan test github

--- mainMenuPage/script.js
+++ mainMenuPage/script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Example value
-  setGaugeValue(gaugeElement, 0.4); // Set this to the student's score percentage (0.2 means 20%)
+  setGaugeValue(gaugeElement, 0.1); // Set this to the student's score percentage (0.2 means 20%)
 
   window.addEventListener('scroll', () => {
     const mainContainer = document.querySelector('.maincontainer');
